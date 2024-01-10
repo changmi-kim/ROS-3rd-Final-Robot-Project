5d2fca1bd744bf360ffa9c0ef6833e61e726be2b
JSeongH <sungho9807@gmail.com>
2024-01-10 21:32:45 +0900

    connect_arduino

--- auto_drive_control/launch/arduino.launch.py
+++ auto_drive_control/launch/arduino.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     param_dir = LaunchConfiguration(
         'param_dir',
         default=os.path.join(
-            get_package_share_directory('arduino_tutorial'),
+            get_package_share_directory('auto_drive_control'),
             'param',
             'arduino_config.yaml'
         )
