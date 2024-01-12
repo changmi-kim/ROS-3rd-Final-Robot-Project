068613948a17010fafac4cc1b1d5513e04516505
JSeongH <sungho9807@gmail.com>
2024-01-12 17:13:16 +0900

    fix2

--- src/call_gazebo/launch/world.launch.py
+++ src/call_gazebo/launch/world.launch.py
@@ -9,8 +9,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+
 
 
 def generate_launch_description():
--- src/description/launch/spawn.launch.py
+++ src/description/launch/spawn.launch.py
@@ -4,22 +4,22 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
-
-import xacro
 
 
 def generate_launch_description():
 
     # Check if we're told to use sim time
     robot_name = LaunchConfiguration('robot_name')
+    robot_x = LaunchConfiguration('robot_x')
+    robot_y = LaunchConfiguration('robot_y')
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('description'))
-    xacro_file = os.path.join(pkg_path,'urdf','robot_coremou.xacro')
+    xacro_file = os.path.join(pkg_path,'urdf','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = Command(['xacro ', xacro_file, ' robot_name:=', robot_name, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
@@ -30,6 +30,37 @@ def generate_launch_description():
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
+    )
+
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_robot',
+        output='screen',
+        arguments=[
+            '-entity', robot_name,
+            '-topic', 'robot_description',
+            '-timeout', '20.0',
+            '-x', robot_x,
+            '-y', robot_y,
+            '-package_to_model'
+        ],
+        prefix="bash -c 'sleep 2.0; $0 $@' ",
+        parameters=[{
+            "use_sim_time": True
+        }],
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+                'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_base_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+                'base_controller'],
+        output='screen'
     )
 
 
@@ -50,5 +81,16 @@ def generate_launch_description():
             default_value='Minibot',
             description='write robot name'),
 
-        node_robot_state_publisher
+        DeclareLaunchArgument(
+            'robot_x',
+            default_value='0.4',
+            description='로봇 x좌표'),
+
+        DeclareLaunchArgument(
+            'robot_y',
+            default_value='0.4',
+            description='로봇 y좌표'),
+
+        node_robot_state_publisher,
+        spawn_robot
     ])
