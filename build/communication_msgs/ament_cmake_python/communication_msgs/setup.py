94762cac055c8bcbd6fa38bb94a79354d8d332c3
JSeongH <sungho9807@gmail.com>
2024-01-13 11:42:51 +0900

    del: build log install

--- build/communication_msgs/ament_cmake_python/communication_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='communication_msgs',
-    version='0.0.0',
-    packages=find_packages(
-        include=('communication_msgs', 'communication_msgs.*')),
-)
--- build/communication_msgs/communication_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jo/final_commit/build/communication_msgs/communication_msgs__py
-# Build directory: /home/jo/final_commit/build/communication_msgs/communication_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
