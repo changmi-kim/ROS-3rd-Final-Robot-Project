94cb34e89a6f4a48d522c20de61497e0ff54a1b6
JSeongH <sungho9807@gmail.com>
2024-01-13 11:50:25 +0900

    del: install log build

--- build/communication/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jo/final_commit/install/communication_msgs/share/communication_msgs/package.sh"
--- install/communication_msgs/local/lib/python3.10/dist-packages/communication_msgs/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from communication_msgs.msg._talker import Talker  # noqa: F401
