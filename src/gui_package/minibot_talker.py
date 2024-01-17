655f0ad8ac3bebe403d3166b026d210af4fe7c02
wintercamo <wintercamo3482@gmail.com>
2024-01-17 15:48:34 +0900

    update_Jan_17_1st

---
+++ src/gui_package/minibot_talker.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException  # 외부에서 실행이 중단 되었을 경우 발생하는 오류
+from rclpy.qos import QoSProfile  # QoS 프로파일 호출
+from rclpy.qos import QoSReliabilityPolicy  # QoS 신뢰성 설정
+# from rclpy.qos import qos_profile_sensor_data  # 센서 데이터 같이 빠르게 변하는 데이터 처리
+from communication_msgs.msg import Talker 
+
+class MinibotTalker(Node):
+
+    def __init__(self):
+        super().__init__('Minibot_talker')
+        qos_profile = QoSProfile(depth=10, 
+                                 reliability=QoSReliabilityPolicy.RELIABLE,
+                                )
+        self.pub = self.create_publisher(Talker, 'minibot_status', qos_profile)
+        self.get_logger().info('Chatting has started. Press Enter to send a message.')
+        self.input_thread_func()
+
+    def input_thread_func(self):
+        msg = Talker()
+        msg.talker_name = "Minibot_talker"
+        msg.message = f"{msg.talker_name} 연결 성공!"
+        self.pub.publish(msg)
+        self.get_logger().info(f'Published: "{msg.message}"')
+
+        while True:
+            self.publish_message(msg)
+
+    def publish_message(self, msg):
+        msg.message = input('Enter your message: ')
+        self.pub.publish(msg)
+        self.get_logger().info(f'Published: "{msg.message}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    talker = MinibotTalker()
+
+    try:
+        rclpy.spin(talker)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+    finally:
+        talker.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
