afa0527d3b76a5d87c56337deb2a789ee4defebb
JSeongH <sungho9807@gmail.com>
2024-01-17 15:19:53 +0900

    delete

--- build/domain_bridge/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jo/final_commit/install/domain_bridge'
--- build/minibot_network/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jo/final_commit/install/minibot_network'
