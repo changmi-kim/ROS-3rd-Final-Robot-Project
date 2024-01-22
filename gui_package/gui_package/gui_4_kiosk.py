43c73c54d866f53b8e7b22aa9d167db9fce1139a
wintercamo <wintercamo3482@gmail.com>
2024-01-23 01:22:54 +0900

    update_Jan_23_1

--- gui_package/gui_package/gui_4_kiosk.py
+++ gui_package/gui_package/gui_4_kiosk.py
@@ -119,35 +119,52 @@ class KioskWindow(QMainWindow, from_class):
     # 결제 요청 버튼
     def pay_confirmed(self):
         # DB 연결
-        self.pay_btn.hide()
-        self.pay_cancel_btn.hide()
 
         self.remote = create_DB_cursor(access_key)
         remote_cursor = self.remote.cursor()
 
         # 해당 로봇 귀환 시킴.
         # 단, 실제 커넥터에서 HUB로 복귀하는 것은 고려하지 않았기 때문에 추후에 쿼리를 수정해야함.
-        sql_query = f'''UPDATE robot_status
-                        SET 
+
+        sql_query = f'''SELECT robot_number from park_system_log
+                        WHERE car_number = "{self.target}"
+                        AND entry_time = '{self.target_entry_time}'
+                        AND charging_end_time = '{self.target_charging_end}'
+                        AND isPayed IS NULL
+                        AND departure_time is NULL
                      '''
+        
+        remote_cursor.execute(sql_query)
+        result = remote_cursor.fetchall()
+        
+        # 하드웨어와 실제로 연동 안됨. 추후에 쿼리 수정해야함.
+        sql_query = f'''UPDATE robot_status
+                        SET status = '사용가능',
+                            request = 'HUB'
+                        WHERE id = {result[0][0]}
+                     '''
+        remote_cursor.execute(sql_query)
         
         # 결제했다고 표시
         sql_query = f'''UPDATE park_system_log
-                        SET isPayed = 'Done'
-                        WHERE car_number = '{self.target}'
+                        SET isPayed = 'Y'
+                        WHERE car_number = "{self.target}"
                         AND entry_time = '{self.target_entry_time}'
-                        AND charging_end_time = '{self.target_charging_end}')
-                        AND price = '{self.price}'
+                        AND charging_end_time = '{self.target_charging_end}'
+                        AND isPayed IS NULL
                         AND departure_time IS NULL
                     '''
         
         remote_cursor.execute(sql_query)
+
         self.remote.commit()
         self.remote.close()
 
-        self.result_pay.setText(f"{self.price}원이 결제 되었습니다.\n이용해주셔서 감사합니다.")
-        
-        self.timer.start(2000)
+        self.phase = 5
+        self.update_kiosk_UI()
+        self.result_mention.setText(f"{self.price}원이 결제 되었습니다.\n이용해주셔서 감사합니다.")
+        QApplication.processEvents()
+        time.sleep(3)
         self.home_btn_click()
 
     def isBotsAvailable(self):
@@ -163,18 +180,20 @@ class KioskWindow(QMainWindow, from_class):
             
         remote_cursor.execute(sql_query)
         result = remote_cursor.fetchall()
+        result = [item[0] for item in result]
 
         if not result:
             # print("로봇확인",result)
-            return None
+            return -1
         else:
-            return result
+            return result[0]
 
     def bot_designation_results(self):
         self.bot_id = self.isBotsAvailable()
-        # 사용가능한 봇이 있으면
+        print(self.bot_id)
 
-        if self.bot_id:
+        # 사용가능한 봇이 있으면
+        if self.bot_id > 0:
             self.remote = create_DB_cursor(access_key)
             remote_cursor = self.remote.cursor()
 
@@ -182,7 +201,7 @@ class KioskWindow(QMainWindow, from_class):
             
             sql_query = f'''UPDATE park_system_log
                             SET charging_start_time = '{now}',
-                                robot_number = '{self.bot_id}',
+                                robot_number = {self.bot_id},
                                 connector = '{self.connector}'
                             WHERE car_number = '{self.target}'
                             AND entry_time = '{self.target_entry_time}'
@@ -195,21 +214,23 @@ class KioskWindow(QMainWindow, from_class):
                             SET status = '사용불가',
                                 request = '{self.connector}',
                                 progress = '이동 중'
-                                WHERE id = {str(self.bot_id)}
+                                WHERE id = {self.bot_id}
                             '''
             remote_cursor.execute(sql_query)
             self.remote.commit()
-            self.remote.close()
+            
         
             self.result_mention.setText(f"{self.target}차량 충전을 위해\n로봇이 {self.connector}에\n배정되었습니다.")
+            self.remote.close()
 
         # 봇이 전부 사용할 수 없는 경우
         else:
             self.result_mention.setText("죄송합니다.\n현재 이용 가능한\n로봇이 없습니다.\n잠시 후에 이용해주세요")
 
-        self.timer.start(4000)
-        
+        QApplication.processEvents()
+        time.sleep(3)
         self.home_btn_click()
+        
 
     # phase4에서 커넥터 현황을 파악하는 함수
     def select_connector(self):
@@ -229,6 +250,7 @@ class KioskWindow(QMainWindow, from_class):
     
         remote_cursor.execute(sql_query)
         self.using_connector = remote_cursor.fetchall()
+        print("사용 중인 커넥터: ", self.using_connector)
         self.using_connector = [item[0] for item in self.using_connector]
 
         for item in self.using_connector:
@@ -255,9 +277,9 @@ class KioskWindow(QMainWindow, from_class):
 
         # 사용 중이 아닌 것을 선택하면 넘어감.
         else:
-            self.bot_designation_results()
             self.phase = 5
             self.update_kiosk_UI()
+            self.bot_designation_results()
 
         self.connetor_alert.setText("잘못된 커넥터를 선택하면\n충전이 되지 않습니다!")
 
@@ -323,9 +345,6 @@ class KioskWindow(QMainWindow, from_class):
             self.result_pay.setText(f"충전 시간은 {charge_time}분이며\n금액은 {self.price}원입니다.")
             
             self.remote.close()
-
-            self.pay_btn.show()
-            self.pay_cancel_btn.show()
         
 
     # phase3에서 조회된 차량 목록 중 자신의 차량을 선택하는 단게
@@ -354,15 +373,16 @@ class KioskWindow(QMainWindow, from_class):
 
         # 충전 시작일 경우? 주차장에 들어와있고, 출차를 안했으며, 충전 시작을 안한 차량.
         if self.start_charing:  
-            sql_query = f'''SELECT * FROM park_system_log WHERE RIGHT(car_number, 4) = "{self.buffer}"
+            sql_query = f'''SELECT car_number, entry_time FROM park_system_log WHERE RIGHT(car_number, 4) = "{self.buffer}"
                             AND entry_time IS NOT NULL
                             AND charging_start_time IS NULL
                             AND departure_time IS NULL'''
         # 충전 종료일 경우? 주차장에 들어와있고, 출차를 안했으며, 충전 시작을 한 차량.
         else:
-            sql_query = f'''SELECT * FROM park_system_log WHERE RIGHT(car_number, 4) = "{self.buffer}"
+            sql_query = f'''SELECT car_number, entry_time FROM park_system_log WHERE RIGHT(car_number, 4) = "{self.buffer}"
                             AND entry_time IS NOT NULL
                             AND charging_start_time IS NOT NULL
+                            AND isPayed IS NULL
                             AND departure_time IS NULL'''
             
         remote_cursor.execute(sql_query)
@@ -383,10 +403,10 @@ class KioskWindow(QMainWindow, from_class):
                 self.parked_list.insertRow(row_num)
 
                 for col_num, col_data in enumerate(row_data):
-                    self.parked_list.setItem(row_num, col_num - 1, QTableWidgetItem(str(col_data)))
+                    self.parked_list.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
 
-                self.phase = 3
-                self.update_kiosk_UI()
+            self.phase = 3
+            self.update_kiosk_UI()
         
         self.remote.close()
 
@@ -439,7 +459,7 @@ class KioskWindow(QMainWindow, from_class):
         try:
             self.remote.close()
         except:
-            pass
+            print("rds 연결끊긴 상태")
         finally:
             self.buffer = ""; self.car_num_buffer.setPlainText(self.buffer)
             self.update_kiosk_UI()
@@ -453,7 +473,7 @@ class KioskWindow(QMainWindow, from_class):
         try:
             self.remote.close()
         except:
-            pass
+            print("rds 연결끊긴 상태")
         finally:
             self.buffer = ""; self.car_num_buffer.setPlainText(self.buffer)
             self.update_kiosk_UI()
